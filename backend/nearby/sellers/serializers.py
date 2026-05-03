3216169585d9c4e1e38dbc414287a645cc326171
anjumaAlam <anjumaalam400@gmail.com>
2026-05-04 03:08:31 +0600

    Add seller backend updates and profile/login frontend pages

--- backend/nearby/sellers/serializers.py
+++ backend/nearby/sellers/serializers.py
@@ -1,5 +1,25 @@
 from rest_framework import serializers
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import Seller
+
+
+class SellerTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['user_type'] = user.user_type
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        if self.user.user_type != 'seller':
+            raise AuthenticationFailed('Only seller accounts can login here.')
+
+        data['user_type'] = self.user.user_type
+        return data
+
 
 class SellerRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -19,3 +39,14 @@ class SellerRegistrationSerializer(serializers.ModelSerializer):
         
         seller = Seller.objects.create_user(**validated_data)
         return seller
+
+
+class SellerProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seller
+        fields = [
+            'full_name', 'username', 'email', 'address', 'phone',
+            'profile_pic', 'business_name', 'neighbourhood',
+            'verification_status', 'rating'
+        ]
+        read_only_fields = ['username', 'email', 'verification_status', 'rating']
--- backend/nearby/sellers/urls.py
+++ backend/nearby/sellers/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 
 urlpatterns = [
     path('register/', views.register_seller, name='register_seller'),
     
-    path('login/', TokenObtainPairView.as_view(), name='seller_login'),
+    path('login/', views.SellerTokenObtainPairView.as_view(), name='seller_login'),
     
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', views.SellerProfileView.as_view(), name='seller_profile'),
 ]
